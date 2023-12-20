be99bc8f4f66e4da6eacab8231f22d93c14102d3
denis miculescu <20098078@mail.wit.ie>
2023-12-20 22:19:38 +0000

    Mongo lab users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -15,11 +16,18 @@ const errHandler = (err, req, res, next) => {
 };
 
 const app = express();
+
 const port = process.env.PORT;
 
+
 app.use(express.json());
+
 app.use('/api/tasks', tasksRouter);
+
 app.use(errHandler);
+
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
