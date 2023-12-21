c2cadfeef271a0385b08df690048554bfc1260c4
denis miculescu <20098078@mail.wit.ie>
2023-12-21 22:36:58 +0000

    Added JWT

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -8,7 +8,30 @@ const UserSchema = new Schema({
   password: {type: String, required: true }
 });
 
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, this.password); 
+}
 
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
+  }
+});
 
 const passwordValidator = (password) => {
     const passwordReq = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
@@ -16,32 +39,6 @@ const passwordValidator = (password) => {
     return passwordReq.test(password);
   };
   
-  UserSchema.path("password").validate(passwordValidator, "Invalid password format.");
-
-
-  UserSchema.methods.comparePassword = async function (passw) { 
-    return await bcrypt.compare(passw, this.password); 
-  }
-  
-  UserSchema.statics.findByUserName = function (username) {
-    return this.findOne({ username: username });
-  };
-  
-  UserSchema.pre('save', async function(next) {
-    const saltRounds = 10; // You can adjust the number of salt rounds
-    //const user = this;
-    if (this.isModified('password') || this.isNew) {
-      try {
-        const hash = await bcrypt.hash(this.password, saltRounds);
-        this.password = hash;
-        next();
-    } catch (error) {
-       next(error);
-    }
-  
-    } else {
-        next();
-    }
-  });
+UserSchema.path("password").validate(passwordValidator, "Invalid password format.");
   
 export default mongoose.model('User', UserSchema);
